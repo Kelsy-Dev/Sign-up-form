98f4b4bb8c35fa2dc5b384f1b8af3ec0d085f83e
KLC <mkelsy@icloud.com>
2022-09-16 02:37:24 -0400

    Fixing form width when field is empty

--- form.js
+++ form.js
@@ -1,87 +1,10 @@
-// const form = document.getElementById('myform');
-// const username = document.getElementById('name');
-// const lastName = document.getElementById('lstname');
-// const email = document.getElementById('email');
-// const password = document.getElementById('password');
-
-// form.addEventListener("submit", (e) => {
-//     e.preventDefault();
-
-//     validateInputs();
-// });
-
-
-// // Defining error messages 
-
-// let errorMessage = (Element, message) => {
-//     const inputControl = element.parentElement;
-//     const errorDisplay = inputControl.querySelector('.error');
-
-//     errorDisplay.innerText = message;
-//     inputControl.classList.add('error');
-//     inputControl.classList.remove('Success');
-// };
-
-// // Set a success message 
-
-// const successMessage = (element) => {
-//      const inputControl = element.parentElement;
-//      const errorDisplay = inputControl.querySelector('.error');
-
-//      errorDisplay.innerText = '';
-//      inputControl.classList.add('error');
-//      inputControl.classList.remove('Success');
-
-// };
-
-// // Validation form
-
-// const validateInputs = () => {
-//     const usernameValue = username.value.trim();
-
-//     if(usernameValue === '') {
-//         console.log("username is required");
-//         errorMessage(username, 'username is required');
-
-//     } else {
-//         console.log("username working");
-//     }
-// }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 const form = document.getElementById('myform');
 const username = document.getElementById('username');
+const lastName = document.getElementById('lstname');
+const mail = document.getElementById('email');
+const password = document.getElementById('password');
+
 
 form.addEventListener('submit', e => {
     e.preventDefault();
@@ -90,27 +13,33 @@ form.addEventListener('submit', e => {
 });
 
 const setError = (element, message) => {
-    const inputControl = element.parentElement;
-    const errorDisplay = inputControl.querySelector('.error');
+    const inputSection = element.parentElement;
+    const errorDisplay = inputSection.querySelector('.error');
 
     errorDisplay.innerText = message;
-    inputControl.classList.add('error');
-    inputControl.classList.remove('success')
+    inputSection.classList.add('errror');
+    inputSection.classList.remove('success')
 }
 
 const setSuccess = element => {
-    const inputControl = element.parentElement;
-    const errorDisplay = inputControl.querySelector('.error');
+    const inputSection = element.parentElement;
+    const errorDisplay = inputSection.querySelector('.error');
 
     errorDisplay.innerText = '';
-    inputControl.classList.add('success');
-    inputControl.classList.remove('error');
+    inputSection.classList.add('success');
+    inputSection.classList.remove('errror');
 };
 
-
+const isValidEmail = email => {
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
 
 const validateInputs = () => {
     const usernameValue = username.value.trim();
+    const lastNameValue = lastName.value.trim();
+    const mailValue = mail.value.trim();
+    const passwordValue = password.value.trim();
 
     if(usernameValue === '') {
         setError(username, 'First name cannot be empty');
@@ -118,4 +47,23 @@ const validateInputs = () => {
         setSuccess(username);
     }
 
+    if(lastNameValue === '') {
+        setError(lastName, 'Last name cannot be empty');
+    } else {
+        setSuccess(lastName);
+    }
+
+    if(mailValue === '') {
+        setError(mail, 'Looks like this is not an email');
+    } else {
+        setSuccess(mail);
+    }
+
+    if(passwordValue === '') {
+        setError(password, 'Password cannot be empty');
+    } else {
+        setSuccess(password);
+    }
 };
+
+
