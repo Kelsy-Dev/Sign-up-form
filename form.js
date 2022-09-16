012cd4b02e27d70d17b7ce480a0f7c6c80ed9631
KLC <mkelsy@icloud.com>
2022-09-16 01:28:19 -0400

    Fixing the validation form

--- form.js
+++ form.js
@@ -1,25 +1,121 @@
-const form = document.getElementById('myform');
-const username = document.getElementById('name');
-const lastName = document.getElementById('lstname');
-const email = document.getElementById('email');
-const password = document.getElementById('password');
+// const form = document.getElementById('myform');
+// const username = document.getElementById('name');
+// const lastName = document.getElementById('lstname');
+// const email = document.getElementById('email');
+// const password = document.getElementById('password');
 
-form.addEventListener("submit", (e) => {
+// form.addEventListener("submit", (e) => {
+//     e.preventDefault();
+
+//     validateInputs();
+// });
+
+
+// // Defining error messages 
+
+// let errorMessage = (Element, message) => {
+//     const inputControl = element.parentElement;
+//     const errorDisplay = inputControl.querySelector('.error');
+
+//     errorDisplay.innerText = message;
+//     inputControl.classList.add('error');
+//     inputControl.classList.remove('Success');
+// };
+
+// // Set a success message 
+
+// const successMessage = (element) => {
+//      const inputControl = element.parentElement;
+//      const errorDisplay = inputControl.querySelector('.error');
+
+//      errorDisplay.innerText = '';
+//      inputControl.classList.add('error');
+//      inputControl.classList.remove('Success');
+
+// };
+
+// // Validation form
+
+// const validateInputs = () => {
+//     const usernameValue = username.value.trim();
+
+//     if(usernameValue === '') {
+//         console.log("username is required");
+//         errorMessage(username, 'username is required');
+
+//     } else {
+//         console.log("username working");
+//     }
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const form = document.getElementById('myform');
+const username = document.getElementById('username');
+
+form.addEventListener('submit', e => {
     e.preventDefault();
 
     validateInputs();
 });
 
-let message = document.getElementsByTagName('body').innerHTML;
+const setError = (element, message) => {
+    const inputControl = element.parentElement;
+    const errorDisplay = inputControl.querySelector('.error');
+
+    errorDisplay.innerText = message;
+    inputControl.classList.add('error');
+    inputControl.classList.remove('success')
+}
+
+const setSuccess = element => {
+    const inputControl = element.parentElement;
+    const errorDisplay = inputControl.querySelector('.error');
+
+    errorDisplay.innerText = '';
+    inputControl.classList.add('success');
+    inputControl.classList.remove('error');
+};
+
+
 
 const validateInputs = () => {
     const usernameValue = username.value.trim();
 
-    if(username.value === '') {
-        console.log("username is required");
-        
-
+    if(usernameValue === '') {
+        setError(username, 'First name cannot be empty');
     } else {
-        console.log("username working");
+        setSuccess(username);
     }
-}
+
+};
